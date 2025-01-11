23e4cf60a0b4110aa7109cd699aad48b988f4416
Frank Capria <fcapria@gmail.com>
2025-01-11 13:56:56 -0500

    Add files via upload
    IoTlib.py contains a Celsius to Fahrenheit conversion formula. sample_mail_config holds the necessary information to email results to the Verizon email to MMS gateway.

---
+++ sample_mail_config.py
@@ -0,0 +1,16 @@
+# Add your information and remove sample_ from the file name in order to use
+# with the temp and daily_check scripts
+
+# Port 465 is the standard SSL port
+# SMTP server example smtp.gmail.com
+# Sender email is an address associated with the SMTP server
+# Receiver email is the address of the email to SMS/MMS gateway
+
+
+config = {
+    "port": 465,
+    "smtpServer": "smtp.gmail.com", 
+    "senderEmail": "",
+    "receiverEmail": "", 
+    "password": ""
+}
